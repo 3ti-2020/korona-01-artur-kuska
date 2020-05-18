30fec9202977510cac912041c6450b640c290d85
Artur <62010277+NiezajetyNick@users.noreply.github.com>
2020-05-18 12:51:20 +0200

    Update dane.php

--- dane.php
+++ dane.php
@@ -1,3 +1,37 @@
 <?php
 
+$servername = "34.77.223.128";
+$username = "stajnie:europe-west1:stajnie";
+$password = "zaq1@WSX";
+$dbname = "kuska";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+$result1 = $conn->query("select id, stajnia, imie_konia, rasa, wiek, dane_wlasciciela, plec from dane, wlasciciele where dane.nr_wlasciciela=wlasciciele.nr_wlasciciela");
+
+echo("<h4>SELECT * FROM pracownicy</h4>");
+echo("<table class='tabelka' border=1>");
+echo("<tr>
+<th>id</th>
+<th>stajnia</th>
+<th>imie konia</th>
+<th>rasa</th>
+<th>wiek</th>
+<th>dane wlasciciela</th>
+<th>plec</th>
+</tr>");
+
+while($row=$result1->fetch_assoc() ){
+      echo("<tr>");
+      echo("<td>".$row['id']."</td>");
+      echo("<td>".$row['stajnia']."</td>");
+      echo("<td>".$row['imie_konia']."</td>");
+      echo("<td>".$row['rasa']."</td>");
+      echo("<td>".$row['wiek']."</td>");
+      echo("<td>".$row['dane_wlasciciela']."</td>");
+      echo("<td>".$row['plec']."</td>");
+      echo("</tr>");
+}
+echo("</table>");
+
 ?>
